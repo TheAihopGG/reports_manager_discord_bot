87d90460edc6ca222da47bd28ffc52b8a8489ad3
theaihopgg <theaihopgg@gmail.com>
2025-07-25 20:22:06 +0700

    fix: bot startup

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,15 @@
-import cogs.reports_admin_commands.cog
-import cogs.reports_member_commands.cog
 from disnake.ext import commands
-from disnake import Intents
 
-from core.configuration import BOT_TOKEN
-from core.logger import logger
+from .cogs.reports_admin_commands.cog import ReportsAdminCommandsCog
+from .cogs.reports_member_commands.cog import ReportsMemberCommandsCog
+from .core.configuration import BOT_TOKEN
+from .core.logger import logger
 
 
-bot = commands.InteractionBot(intents=Intents.all())
+bot = commands.InteractionBot()
 
-bot.add_cog(cogs.reports_admin_commands.cog.ReportsAdminCommandsCog())
-bot.add_cog(cogs.reports_member_commands.cog.ReportsMemberCommandsCog())
+bot.add_cog(ReportsAdminCommandsCog())
+bot.add_cog(ReportsMemberCommandsCog())
 
 
 @bot.event
@@ -18,4 +17,5 @@ async def on_ready() -> None:
     logger.info("Bot successfully launched")
 
 
-bot.run(BOT_TOKEN)
+def main() -> None:
+    bot.run(BOT_TOKEN)
