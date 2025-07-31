3e6964feefa3fcc6ce039a4157b65fb711e49174
theaihopgg <theaihopgg@gmail.com>
2025-07-31 20:15:41 +0700

    fix: reports config

--- bot/core/models/reports_config.py
+++ bot/core/models/reports_config.py
@@ -5,9 +5,6 @@ from sqlalchemy.types import JSON
 from .base import BaseModel
 from .mixins import IDMixin, GuildIDMixin
 
-if TYPE_CHECKING:
-    from ...services.logs_service.logs_types import LogsTypesEnum
-
 
 class ReportsConfigModel(
     BaseModel,
@@ -23,16 +20,9 @@ class ReportsConfigModel(
     is_logs_enabled: Mapped[bool] = mapped_column(nullable=False, default=True)
     logs_channel_id: Mapped[int] = mapped_column(nullable=True, default=None)
     report_tickets_category_id: Mapped[int] = mapped_column(nullable=True, default=None)
-    enabled_logs_types: Mapped[dict[LogsTypesEnum, bool]] = mapped_column(
+    enabled_logs_types: Mapped[dict[str, bool]] = mapped_column(
         JSON(none_as_null=True),
-        default=lambda: {
-            LogsTypesEnum.REPORT_CLOSED: True,
-            LogsTypesEnum.REPORT_SENDED: True,
-            LogsTypesEnum.MEMBER_ADDED_TO_REPORT_TICKET: True,
-            LogsTypesEnum.MEMBER_REMOVED_FROM_REPORT_TICKET: True,
-            LogsTypesEnum.REPORTS_ADMIN_ADDED: True,
-            LogsTypesEnum.REPORTS_ADMIN_REMOVED: True,
-        },
+        default=lambda: {},
     )
 
 
