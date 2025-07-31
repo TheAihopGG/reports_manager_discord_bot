5bc8d47f2f77033b96d0c0953d59c547298acc98
theaihopgg <theaihopgg@gmail.com>
2025-07-31 20:11:08 +0700

    fix: logger

--- bot/core/logger.py
+++ bot/core/logger.py
@@ -4,18 +4,20 @@ Module contains `logger` object to interact with bot logs
 
 from logging import FileHandler, StreamHandler, getLogger, basicConfig
 
-logger = getLogger(__name__)
+from .configuration import LOGGING_FILENAME
+
 basicConfig(
     format="%(asctime)s [%(levelname)s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[
         StreamHandler(),
         FileHandler(
-            filename="",
+            filename=LOGGING_FILENAME,
             mode="w",
             encoding="utf-8",
         ),
     ],
 )
+logger = getLogger(__file__)
 
 __all__ = ("logger",)
